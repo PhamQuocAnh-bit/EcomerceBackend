4db0f80f374dc4fa5162bc80526e324ef1389c94
PhamQuocAnh <anhpham030704@gmail.com>
2026-07-27 09:25:27 +0700

    add review and wishlist

--- techstore/src/main/java/com/techstore/TechstoreApplication.java
+++ techstore/src/main/java/com/techstore/TechstoreApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TechstoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechstoreApplication.class, args);
+
+        SpringApplication.run(TechstoreApplication.class, args);
 	}
 
 }
